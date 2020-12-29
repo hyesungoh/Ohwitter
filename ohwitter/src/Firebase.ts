305413e377118e19f39e58e5b63270ca942ad6e0
hyesungoh <haesungoh414@gmail.com>
2020-12-30 02:45:32 +0900

    add firebase SDK

---
+++ ohwitter/src/Firebase.ts
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import * as config from "./Config";
+
+const firebaseConfig: object = {
+    apiKey: config.FIREBASE_API_KEY,
+    authDomain: config.FIREBASE_AUTH_DOMAIN,
+    projectId: config.FIREBASE_PROJECT_ID,
+    storageBucket: config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: config.FIREBASE_MESSAGING_SENDER_ID,
+    appId: config.FIREBASE_APP_ID,
+};
+
+export default firebase.initializeApp(firebaseConfig);
