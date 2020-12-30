d1269599394f8cb397c8dde6062da84b0996998d
hyesungoh <haesungoh414@gmail.com>
2020-12-30 21:44:14 +0900

    Apply .gitignore

--- ohwitter/src/App.tsx
+++ ohwitter/src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <h1>hello?</h1>
+        </div>
+    );
 }
 
 export default App;
